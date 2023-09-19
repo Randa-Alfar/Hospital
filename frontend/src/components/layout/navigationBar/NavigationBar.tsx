e94ee36443ea37ba037b76516701403ceed701b7
Randa-Alfar <my.violin.ru@gmail.com>
2023-09-19 23:33:11 +0300

    add the frontend folder

---
+++ frontend/src/components/layout/navigationBar/NavigationBar.tsx
@@ -0,0 +1,11 @@
+import { ReactElement, ReactNode } from "react";
+
+const NavigationBar= () => {
+    return (
+        <div id="navigationBarContainer">
+            <h1>  any thing that we can write </h1>
+        </div>
+    );
+}
+
+export default NavigationBar;
---
+++ frontend/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import router from './Main';
+import { RouterProvider } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
